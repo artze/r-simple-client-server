7f023b9d869db1fde5d879ee155257abd23362fb
artze <arthurwtm@gmail.com>
2019-02-26 17:47:23 +0000

    Refactor transmission sequence with async lib

--- sender/transmit.js
+++ sender/transmit.js
@@ -1,19 +1,38 @@
 'use strict'
 
 const net = require('net');
+const async = require('async');
 const client = new net.Socket();
 const port = 8080;
 const host = '127.0.0.1';
+
 
 /*
 *  This function will be called for each event.  (eg: for each sensor reading)
 *  Modify it as needed.
 */
 module.exports = (eventMsg, encoding, callback) => {
-  client.connect(port, host, () => {
-    client.write(JSON.stringify(eventMsg), (err) => {
-      client.destroy()
-      callback(err)
-    })
+  const transmissionSequence = [
+    // 1. Establish TCP connection
+    (callback) => {
+      client.connect(port, host, () => {
+        callback(null)
+      })
+    },
+    // 2. Transmit message
+    (callback) => {
+      client.write(JSON.stringify(eventMsg), (err) => {
+        callback(err)
+      })
+    },
+    // 3. Close TCP connection
+    (callback) => {
+      client.destroy();
+      callback(null);
+    }
+  ]
+  
+  async.series(transmissionSequence, function(err) {
+    callback(err)
   })
 }
