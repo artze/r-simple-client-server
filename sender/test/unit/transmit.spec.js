ba019b2b735c7bb68ce25a1d26a7fa4a27b387df
artze <arthurwtm@gmail.com>
2019-02-28 16:58:48 +0000

    Test TCP client destroy method is called correctly

--- sender/test/unit/transmit.spec.js
+++ sender/test/unit/transmit.spec.js
@@ -1,4 +1,7 @@
-const expect = require('chai').expect;
+/**
+ * Test package for transmit.js module
+ * IMPORTANT NOTE: this test package assumes that transmit.js module makes TCP transmission at 2-second intervals
+ */
 const sinon = require('sinon');
 const transmit = require('../../transmit');
 const transmissionData = require('../fixtures/transmissionData');
@@ -24,10 +27,11 @@ describe('Test transmit module', () => {
     clock.tick(2000 * transmissionData.length);
     connect.restore();
     clock.restore();
+
     sinon.assert.callCount(connect, Math.ceil(transmissionData.length / 2));
   })
 
-  it('Should write onto TCP connection with correct message', () => {
+  it('Should call client write method with correct message', () => {
     const connect = sinon.stub(transmit.testHelpers.client, 'connect');
     const write = sinon.stub(transmit.testHelpers.client, 'write');
     const clock = sinon.useFakeTimers();
@@ -47,10 +51,42 @@ describe('Test transmit module', () => {
     
     clock.tick(2000 * transmissionData.length);
     connect.yield();
+    connect.restore();
     write.restore();
     clock.restore();
+
     sinon.assert.callCount(write, Math.ceil(transmissionData.length / 2));
     sinon.assert.calledWith(write.firstCall, transmit.testHelpers.generateMsgString(transmissionData.slice(0, 2)))
     sinon.assert.calledWith(write.lastCall, transmit.testHelpers.generateMsgString(transmissionData.slice(-1)))
+  })
+
+  it('Should call destroy TCP client method correctly', () => {
+    const connect = sinon.stub(transmit.testHelpers.client, 'connect');
+    const write = sinon.stub(transmit.testHelpers.client, 'write');
+    const destroy = sinon.stub(transmit.testHelpers.client, 'destroy');
+    const clock = sinon.useFakeTimers();
+    const eventInterval = 1000;
+
+    let index = 0
+    const transmitAtInterval = () => {
+      setTimeout(() => {
+        transmit(transmissionData[index], null, () => {});
+        index++;
+        if(index < transmissionData.length) {
+          transmitAtInterval();
+        }
+      }, eventInterval);
+    }
+    transmitAtInterval();
+    
+    clock.tick(2000 * transmissionData.length);
+    connect.yield();
+    write.yield();
+    connect.restore();
+    write.restore();
+    destroy.restore();
+    clock.restore();
+
+    sinon.assert.callCount(destroy, Math.ceil(transmissionData.length / 2));
   })
 })
